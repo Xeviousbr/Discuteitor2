6d4714a71b2e60038baf15a66ea04e153a140f4b
Xeviousbr <xeviousbr@gmail.com>
2022-04-19 12:47:06 -0300

    Adicionar arquivos de projeto.

---
+++ Discuteitor/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Discuteitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.lbTempo = new System.Windows.Forms.Label();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.Appearance = System.Windows.Forms.Appearance.Button;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(12, 35);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(106, 30);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Minha Vez";
+            this.radioButton1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.Click += new System.EventHandler(this.radioButton1_Click);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.Appearance = System.Windows.Forms.Appearance.Button;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(234, 35);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(106, 30);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Vez Dela";
+            this.radioButton2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.Click += new System.EventHandler(this.radioButton2_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // lbTempo
+            // 
+            this.lbTempo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTempo.Location = new System.Drawing.Point(12, 6);
+            this.lbTempo.Name = "lbTempo";
+            this.lbTempo.Size = new System.Drawing.Size(325, 26);
+            this.lbTempo.TabIndex = 6;
+            this.lbTempo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.Appearance = System.Windows.Forms.Appearance.Button;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton3.Location = new System.Drawing.Point(122, 35);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(106, 30);
+            this.radioButton3.TabIndex = 7;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Pausa";
+            this.radioButton3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.Click += new System.EventHandler(this.radioButton3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 74);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.lbTempo);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Discuteitor";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label lbTempo;
+        private System.Windows.Forms.RadioButton radioButton3;
+    }
+}
+
---
+++ Discuteitor/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Media;
+using System.Windows.Forms;
+
+// 1 - Quando clicar em pausa passar quem fala para "Parar"
+// 1.1 - Se clicar em "Parar", passar para "Falar" e o outro também, e o 'continuar' retornar para "Pausa"
+
+// 2 - Quando para de falar, atinge o zero, mas não faz nada, para o timer
+
+    // 3 - Quando clicar em se inscrever, deve mostrar a opção de Sair da inscrição
+    // mas ainda deve ter também o botão de parar
+    // então tem que aparecer os dois botões
+    // portanto tem que ter uma alteraçãod e interface
+
+/* Bug1 
+    Cliquei pra Dayse falar
+    e me inscrevi
+
+    Falei
+    ela se inscreveu
+
+    Falou
+
+    eu cliquei (não lembro como tava)
+    E não mudou pra me inscrever
+    (ficou aparecendo falar) */
+
+namespace Discuteitor
+{
+public partial class Form1 : Form
+{
+    private int vez = -1;
+    private int tempofinal = 0;
+    private int tempo = 0;
+    private int Inscrito = -1;
+    private bool EmFlash = false;
+
+    // private int TmpFinalMult = 10;
+    private int TmpFinalMult = 61;
+
+    private bool Ativou = false;
+
+    public Form1()
+    {
+        InitializeComponent();
+        //this.TopMost = true;
+    }
+
+    private void Inicializar()
+    {
+        tempo = 0;
+        tempofinal = TmpFinalMult;
+        timer.Enabled = true;
+        radioButton3.Text = "Pausa";
+        TmpFinalMult--;
+    }
+
+    private void Terminou()
+    {
+        timer.Enabled = false;
+        if (Inscrito > -1)
+        {
+            string sSom = "";
+            if (Inscrito==0)
+            {
+                radioButton1.Text = "Falar";
+                radioButton2.Text = "Se inscrever";
+                sSom = "Vez do Arnaldo.wav";
+            } else
+            {
+                radioButton1.Text = "Se inscrever";
+                radioButton2.Text = "Falar";
+                sSom = "Vez da Dayse.wav";
+            }
+            SoundPlayer simpleSound = new SoundPlayer(sSom);
+            simpleSound.Play();
+            FlashWindow.Start(this);
+            EmFlash = true;
+        }
+    }
+
+    private void timer_Tick(object sender, EventArgs e)
+    {
+        this.tempo++;
+        int mostrar = tempofinal - tempo;
+        string m = "";
+        if (mostrar>60)
+        {
+            int min = (mostrar / 60);
+            m = min.ToString()+":";
+            mostrar -= min * 60;
+        }
+        if (mostrar > -0.9)
+        {
+            m += mostrar.ToString();
+            lbTempo.Text = m;
+        } else
+            Terminou();
+    }
+
+    private void radioButton3_Click(object sender, EventArgs e)
+    {
+        if (timer.Enabled)
+            radioButton3.Text = "Continuar";
+        else
+            radioButton3.Text = "Pausa";
+        timer.Enabled = !timer.Enabled;
+        TiraFlash();
+    }
+
+    private void radioButton1_Click(object sender, EventArgs e)
+    {
+        if (Ativou)
+        {
+            vez = 0;
+            OperBotoes(radioButton1);
+        } else
+            Ativou = true;
+    }
+
+    private void radioButton2_Click(object sender, EventArgs e)
+    {
+        vez = 1;
+        OperBotoes(radioButton2);
+    }
+
+    private void OperBotoes(RadioButton RD)
+    {
+        if (RD.Text == "Se Inscrever")
+        {
+            RD.Text = "Falar";
+            Inscrito = vez;
+            Inicializar();
+        }
+        else
+        {
+            string c1 = "";
+            string c0 = "";
+            if (vez == 0)
+            {
+                c0 = "Falando";
+                c1 = "Se Inscrever";
+            }
+            else
+            {
+                c0 = "Se Inscrever";
+                c1 = "Falando";                
+                
+            }
+            radioButton1.Text = c0;
+            radioButton2.Text = c1;
+            tempo = 0;
+            lbTempo.Text = "";
+            TmpFinalMult--;
+            tempofinal = TmpFinalMult;
+            timer.Enabled = false;
+        }
+        TiraFlash();
+    }
+
+    private void TiraFlash()
+    {
+        if (EmFlash)
+        {
+            FlashWindow.Stop(this);
+            EmFlash = false;
+        }
+    }
+}
+}
